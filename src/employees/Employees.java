83bff08d1fa7500c2ff2443e4c9de0fd72ba6eb0
Agul Krishnan <Agul Krishnan@AgulKrishnan-PC>
2020-11-02 17:19:38 +0530

    This is a employee details program

--- src/employees/Employees.java
+++ src/employees/Employees.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package employees;
 
 import java.util.ArrayList;
@@ -13,7 +8,7 @@ import java.util.Scanner;
  * @author Agul Krishnan
  */
 
-class EmployeeData{
+abstract class EmployeeData{
     int id;
     String name;
     int age;
